f2aee78f5d76cb81ae5a0b3c0ec2ab5d360b96e8
dg12988 <doug.goldberg@me.com>
2021-07-06 06:26:31 -0400

    Update Links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ export class AppComponent{
     window.open("https://web.simsol.com/eforms/claimswire-mapping-beta-feedback/61/","_blank");
   }
   onDone(){
-    window.location.href = 'https://claimswire-dev.simsol.com/claimswire/app/assignments';
+    window.location.href = 'https://REDACTED.simsol.com/claimswire/app/assignments';
   }
 }
   
